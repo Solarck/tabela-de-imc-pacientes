d32a1f23e72d914098f5161a8f864e7b33a13194
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2022-12-07 19:28:51 -0300

    up

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let titulo = document.querySelector('#titulo')
-	console.log(titulo.textContent)
-	titulo.textContent = 'Aparecida Nutricionista'
+console.log(titulo.textContent)
+titulo.textContent = 'Aparecida Nutricionista'
 
-    let paciente = document.querySelector('#primeiro-paciente')
+let pacientes = document.querySelectorAll('.paciente')
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i]
+
     let tdpeso = paciente.querySelector('.info-peso')
     let peso = tdpeso.textContent
     let pesovalido = true
@@ -13,20 +18,26 @@ let titulo = document.querySelector('#titulo')
 
     let infoimc = paciente.querySelector('.info-imc')
 
-    if(peso < 0 || peso > 1000){
+    if (peso < 0 || peso > 1000) {
         console.log('Peso invalido')
-        pesovalido  = false
+        pesovalido = false
         infoimc.textContent = 'Peso invalido'
+
+        paciente.classList.add("paciente-invalido")
     }
 
-    if(altura < 0 || altura > 3.00){
+    if (altura < 0 || altura > 3.00) {
         console.log('Altura invalida')
         alturavalida = false
         infoimc.textContent = 'Altura invalida'
+
+        paciente.classList.add("paciente-invalido");
     }
 
-    if(pesovalido && alturavalida ){
-        let imc = peso / ( altura * altura )
-        infoimc.textContent = imc
+    if (pesovalido && alturavalida) {
+        let imc = peso / (altura * altura)
+        infoimc.textContent = imc.toFixed(2)
     }
+
+}
 
